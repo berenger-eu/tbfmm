7134fef0e5f382c852efa866803e274338fc8da1
Berenger Bramas <Berenger.Bramas@inria.fr>
2020-01-22 15:35:45 +0100

    Make it compile without FFTW

--- unit-tests/utest-unifkernel-float.cpp
+++ unit-tests/utest-unifkernel-float.cpp
@@ -2,6 +2,10 @@
 #include "algorithms/sequential/tbfalgorithm.hpp"
 #include "kernels/unifkernel/FUnifKernel.hpp"
 
+// -- DOT NOT REMOVE AS LONG AS LIBS ARE USED --
+// @TBF_USE_FFTW
+// -- END --
+
 // You must do this
 using AlgoTestClass = TestUnifKernel<float, TbfAlgorithm>;
 TestClass(AlgoTestClass)
--- unit-tests/utest-unifkernel-spetabaru-float.cpp
+++ unit-tests/utest-unifkernel-spetabaru-float.cpp
@@ -2,6 +2,10 @@
 #include "algorithms/smspetabaru/tbfsmspetabarualgorithm.hpp"
 #include "kernels/unifkernel/FUnifKernel.hpp"
 
+// -- DOT NOT REMOVE AS LONG AS LIBS ARE USED --
+// @TBF_USE_FFTW
+// -- END --
+
 // You must do this
 using AlgoTestClass = TestUnifKernel<float, TbfSmSpetabaruAlgorithm>;
 TestClass(AlgoTestClass)
