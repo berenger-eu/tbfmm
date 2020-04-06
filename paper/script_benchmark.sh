9b26aaf20d0514732f0f97d15a6ee0c6c6cd16cf
Berenger Bramas <Berenger.Bramas@inria.fr>
2020-04-06 13:58:40 +0200

    Update script

--- paper/script_benchmark.sh
+++ paper/script_benchmark.sh
@@ -3,27 +3,28 @@
 function execute(){
     all_lines="nb_particles,height,nb_threads,block_size,exec_time,build_time,algo_name"
     
-    for nb_particles_height in "1000000:5" "10000000:6" ; do
+    for nb_particles_height in "1000000:5:128" "10000000:6:256" ; do
         nb_particles=$(echo $nb_particles_height | cut -d':' -f 1)
         height=$(echo $nb_particles_height | cut -d':' -f 2)
+        block_size=$(echo $nb_particles_height | cut -d':' -f 3)
         
         echo "nb_particles $nb_particles"
         echo "height $height"
         
         for nb_threads in 1 2 4 8 16 32 ; do
             echo "$nb_threads"
-            result=$(TBFMM_BLOCK_SIZE=128 OMP_PROC_BIND=true OMP_NUM_THREADS=$nb_threads ./testUnifKernel -nb $nb_particles -th $height -nc)
+            result=$(TBFMM_BLOCK_SIZE=$block_size OMP_PROC_BIND=true OMP_NUM_THREADS=$nb_threads ./testUnifKernel -nb $nb_particles -th $height -nc)
             exec_time=$(echo "$result" | grep "Execute FMM in" | cut -d' ' -f 4 | cut -d's' -f 1)
             build_time=$(echo "$result" | grep "Build the tree in" | cut -d' ' -f 5 | cut -d's' -f 1)
             algo_name=$(echo "$result" | grep "Algorithm name" | cut -d' ' -f 3)
                         
-            line="$nb_particles,$height,$nb_threads,128,$exec_time,$build_time,$algo_name"
+            line="$nb_particles,$height,$nb_threads,$block_size,$exec_time,$build_time,$algo_name"
             echo "$line"
             all_lines="$all_lines\n$line"
         done
     done
     
-    echo -e "$all_lines" > "timings.csv"
+    echo -e "$all_lines" >> "timings.csv"
 }
 
 execute;
