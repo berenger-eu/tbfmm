ce0bca31a223564406a2fc1afd5403008aa19278
Berenger Bramas <Berenger.Bramas@inria.fr>
2023-10-03 13:31:24 +0200

    move callbacks in a file

--- src/algorithms/smstarpu/tbfsmstarpualgorithm.hpp
+++ src/algorithms/smstarpu/tbfsmstarpualgorithm.hpp
@@ -6,7 +6,7 @@
 #include "../sequential/tbfgroupkernelinterface.hpp"
 #include "spacial/tbfspacialconfiguration.hpp"
 #include "algorithms/tbfalgorithmutils.hpp"
-
+#include "tbfsmstarpucallbacks.hpp"
 
 #include <cassert>
 #include <iterator>
@@ -45,199 +45,12 @@ protected:
     starpu_codelet p2p_cl_oneleaf;
     starpu_codelet p2p_cl_twoleaves;
 
-    template<class CellContainerClass, class ParticleContainerClass>
-    static void P2MCallback(void *buffers[], void *cl_arg){
-        ThisClass* thisptr;
-        starpu_codelet_unpack_args(cl_arg, &thisptr);
-
-        unsigned char* particleData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[0]);
-        size_t particleDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[0]);
-
-        unsigned char* leafData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[1]);
-        size_t leafDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[1]);
-
-        unsigned char* leafMultipole = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[2]);
-        size_t leafMultipoleSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[2]);
-
-        CellContainerClass leafGroupObj(leafData, leafDataSize,
-                                        leafMultipole, leafMultipoleSize,
-                                        nullptr, 0);
-        const ParticleContainerClass particleGroupObj(particleData, particleDataSize,
-                                                nullptr, 0);
-
-        thisptr->kernelWrapper.P2M(thisptr->kernels[starpu_worker_get_id()], particleGroupObj, leafGroupObj);
-    }
-
-    template<class ParticleContainerClass>
-    static void P2PBetweenLeavesCallback(void *buffers[], void *cl_arg){
-        ThisClass* thisptr;
-        VecOfIndexes* indexesForGroup_first;
-        starpu_codelet_unpack_args(cl_arg, &thisptr, &indexesForGroup_first);
-
-        unsigned char* srcData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[0]);
-        size_t srcDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[0]);
-
-        unsigned char* srcRhs = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[1]);
-        size_t srcRhsSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[1]);
-
-        unsigned char* tgtData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[2]);
-        size_t tgtDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[2]);
-
-        unsigned char* tgtRhs = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[3]);
-        size_t tgtRhsSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[3]);
-
-        ParticleContainerClass groupSrc(srcData, srcDataSize,
-                                        srcRhs, srcRhsSize);
-        ParticleContainerClass groupTarget(tgtData, tgtDataSize,
-                                           tgtRhs, tgtRhsSize);
-
-        thisptr->kernelWrapper.P2PBetweenGroups(thisptr->kernels[starpu_worker_get_id()], groupTarget, groupSrc, *indexesForGroup_first);
-    }
-
-    template<class ParticleContainerClass>
-    static void P2POneLeafCallback(void *buffers[], void *cl_arg){
-        ThisClass* thisptr;
-        VecOfIndexes* indexesForGroup_first;
-        starpu_codelet_unpack_args(cl_arg, &thisptr, &indexesForGroup_first);
-
-        unsigned char* particleData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[0]);
-        size_t particleDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[0]);
-
-        unsigned char* particleRhs = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[1]);
-        size_t particleRhsSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[1]);
-
-        ParticleContainerClass currentGroup(particleData, particleDataSize,
-                                            particleRhs, particleRhsSize);
-
-        thisptr->kernelWrapper.P2PInGroup(thisptr->kernels[starpu_worker_get_id()], currentGroup, *indexesForGroup_first);
-        thisptr->kernelWrapper.P2PInner(thisptr->kernels[starpu_worker_get_id()], currentGroup);
-    }
-
-    template<class CellContainerClass>
-    static void M2MCallback(void *buffers[], void *cl_arg){
-        ThisClass* thisptr;
-        long int idxLevel;
-        starpu_codelet_unpack_args(cl_arg, &thisptr, &idxLevel);
-
-        unsigned char* lowerData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[0]);
-        size_t lowerDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[0]);
-
-        unsigned char* lowerMultipole = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[1]);
-        size_t lowerMultipoleSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[1]);
-
-        unsigned char* upperData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[2]);
-        size_t upperDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[2]);
-
-        unsigned char* upperMultipole = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[3]);
-        size_t upperMultipoleSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[3]);
-
-        const CellContainerClass lowerGroupObj(lowerData, lowerDataSize, lowerMultipole, lowerMultipoleSize,
-                                               nullptr, 0);
-        CellContainerClass upperGroupObj(upperData, upperDataSize, upperMultipole, upperMultipoleSize,
-                                         nullptr, 0);
-
-        thisptr->kernelWrapper.M2M(idxLevel, thisptr->kernels[starpu_worker_get_id()], lowerGroupObj, upperGroupObj);
-    }
-
-    template<class CellContainerClass>
-    static void M2LCallback(void *buffers[], void *cl_arg){
-        ThisClass* thisptr;
-        int idxLevel;
-        VecOfIndexes* indexesForGroup_first;
-        starpu_codelet_unpack_args(cl_arg, &thisptr, &idxLevel, &indexesForGroup_first);
-
-        unsigned char* srcData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[0]);
-        size_t srcDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[0]);
-
-        unsigned char* srcMultipole = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[1]);
-        size_t srcMultipoleSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[1]);
-
-        unsigned char* tgtData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[2]);
-        size_t tgtDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[2]);
-
-        unsigned char* tgtLocal = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[3]);
-        size_t tgtLocalSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[3]);
-
-        const CellContainerClass groupSrc(srcData, srcDataSize, srcMultipole, srcMultipoleSize,
-                                          nullptr, 0);
-        CellContainerClass groupTarget(tgtData, tgtDataSize, nullptr, 0, tgtLocal, tgtLocalSize);
-
-        thisptr->kernelWrapper.M2LBetweenGroups(idxLevel, thisptr->kernels[starpu_worker_get_id()], groupTarget, groupSrc, *indexesForGroup_first);
-    }
-
-    template<class CellContainerClass>
-    static void M2LInnerCallback(void *buffers[], void *cl_arg){
-        ThisClass* thisptr;
-        int idxLevel;
-        VecOfIndexes* indexesForGroup_first;
-        starpu_codelet_unpack_args(cl_arg, &thisptr, &idxLevel, &indexesForGroup_first);
-
-        unsigned char* srcData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[0]);
-        size_t srcDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[0]);
-
-        unsigned char* srcMultipole = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[1]);
-        size_t srcMultipoleSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[1]);
-
-        unsigned char* srcLocal = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[2]);
-        size_t srcLocalSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[2]);
-
-        CellContainerClass currentGroup(srcData, srcDataSize, srcMultipole, srcMultipoleSize,
-                                         srcLocal, srcLocalSize);
-
-        thisptr->kernelWrapper.M2LInGroup(idxLevel, thisptr->kernels[starpu_worker_get_id()], currentGroup, *indexesForGroup_first);
-    }
-
-    template<class CellContainerClass>
-    static void L2LCallback(void *buffers[], void *cl_arg){
-        ThisClass* thisptr;
-        long int idxLevel;
-        starpu_codelet_unpack_args(cl_arg, &thisptr, &idxLevel);
-
-        unsigned char* upperData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[0]);
-        size_t upperDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[0]);
-
-        unsigned char* upperLocal = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[1]);
-        size_t upperLocalSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[1]);
-
-        unsigned char* lowerData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[2]);
-        size_t lowerDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[2]);
-
-        unsigned char* lowerLocal = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[3]);
-        size_t lowerLocalSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[3]);
-
-        const CellContainerClass upperGroupObj(upperData, upperDataSize, nullptr, 0, upperLocal, upperLocalSize);
-        CellContainerClass lowerGroupObj(lowerData, lowerDataSize, nullptr, 0, lowerLocal, lowerLocalSize);
-
-        thisptr->kernelWrapper.L2L(idxLevel, thisptr->kernels[starpu_worker_get_id()], upperGroupObj, lowerGroupObj);
-    }
-
-    template<class CellContainerClass, class ParticleContainerClass>
-    static void L2PCallback(void *buffers[], void *cl_arg){
-        ThisClass* thisptr;
-        starpu_codelet_unpack_args(cl_arg, &thisptr);
-
-        unsigned char* leafData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[0]);
-        size_t leafDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[0]);
-
-        unsigned char* leafLocal = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[1]);
-        size_t leafLocalSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[1]);
-
-        unsigned char* particleData = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[2]);
-        size_t particleDataSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[2]);
-
-        unsigned char* particleRhs = (unsigned char*)STARPU_VARIABLE_GET_PTR(buffers[3]);
-        size_t particleRhsSize = STARPU_VARIABLE_GET_ELEMSIZE(buffers[3]);
-
-        const CellContainerClass leafGroupObj(leafData, leafDataSize, nullptr, 0, leafLocal, leafLocalSize);
-        ParticleContainerClass particleGroupObj(particleData, particleDataSize, particleRhs, particleRhsSize);
-
-        thisptr->kernelWrapper.L2P(thisptr->kernels[starpu_worker_get_id()], leafGroupObj, particleGroupObj);
-    }
+    friend TbfSmStarpuCallbacks;
 
     template<class CellContainerClass, class ParticleContainerClass>
     void initCodelet(){
         memset(&p2m_cl, 0, sizeof(p2m_cl));
-        p2m_cl.cpu_funcs[0] = &P2MCallback<CellContainerClass, ParticleContainerClass>;
+        p2m_cl.cpu_funcs[0] = &TbfSmStarpuCallbacks::P2MCallback<ThisClass, CellContainerClass, ParticleContainerClass>;
         p2m_cl.where |= STARPU_CPU;
         p2m_cl.nbuffers = 3;
         p2m_cl.modes[0] = STARPU_R;
@@ -246,7 +59,7 @@ protected:
         p2m_cl.name = "p2m_cl";
 
         memset(&m2m_cl, 0, sizeof(m2m_cl));
-        m2m_cl.cpu_funcs[0] = &M2MCallback<CellContainerClass>;
+        m2m_cl.cpu_funcs[0] = &TbfSmStarpuCallbacks::M2MCallback<ThisClass, CellContainerClass>;
         m2m_cl.where |= STARPU_CPU;
         m2m_cl.nbuffers = 4;
         m2m_cl.modes[0] = STARPU_R;
@@ -256,7 +69,7 @@ protected:
         m2m_cl.name = "m2m_cl";
 
         memset(&l2l_cl, 0, sizeof(l2l_cl));
-        l2l_cl.cpu_funcs[0] = &L2LCallback<CellContainerClass>;
+        l2l_cl.cpu_funcs[0] = &TbfSmStarpuCallbacks::L2LCallback<ThisClass, CellContainerClass>;
         l2l_cl.where |= STARPU_CPU;
         l2l_cl.nbuffers = 4;
         l2l_cl.modes[0] = STARPU_R;
@@ -266,7 +79,7 @@ protected:
         l2l_cl.name = "l2l_cl";
 
         memset(&l2p_cl, 0, sizeof(l2p_cl));
-        l2p_cl.cpu_funcs[0] = &L2PCallback<CellContainerClass, ParticleContainerClass>;
+        l2p_cl.cpu_funcs[0] = &TbfSmStarpuCallbacks::L2PCallback<ThisClass, CellContainerClass, ParticleContainerClass>;
         l2p_cl.where |= STARPU_CPU;
         l2p_cl.nbuffers = 4;
         l2p_cl.modes[0] = STARPU_R;
@@ -276,7 +89,7 @@ protected:
         l2p_cl.name = "l2p_cl";
 
         memset(&p2p_cl_oneleaf, 0, sizeof(p2p_cl_oneleaf));
-        p2p_cl_oneleaf.cpu_funcs[0] = &P2POneLeafCallback<ParticleContainerClass>;
+        p2p_cl_oneleaf.cpu_funcs[0] = &TbfSmStarpuCallbacks::P2POneLeafCallback<ThisClass, ParticleContainerClass>;
         p2p_cl_oneleaf.where |= STARPU_CPU;
         p2p_cl_oneleaf.nbuffers = 2;
         p2p_cl_oneleaf.modes[0] = STARPU_R;
@@ -284,7 +97,7 @@ protected:
         p2p_cl_oneleaf.name = "p2p_cl_oneleaf";
 
         memset(&p2p_cl_twoleaves, 0, sizeof(p2p_cl_twoleaves));
-        p2p_cl_twoleaves.cpu_funcs[0] = &P2PBetweenLeavesCallback<ParticleContainerClass>;
+        p2p_cl_twoleaves.cpu_funcs[0] = &TbfSmStarpuCallbacks::P2PBetweenLeavesCallback<ThisClass, ParticleContainerClass>;
         p2p_cl_twoleaves.where |= STARPU_CPU;
         p2p_cl_twoleaves.nbuffers = 4;
         p2p_cl_twoleaves.modes[0] = STARPU_R;
@@ -294,7 +107,7 @@ protected:
         p2p_cl_twoleaves.name = "p2p_cl_twoleaves";
 
         memset(&m2l_cl_between_groups, 0, sizeof(m2l_cl_between_groups));
-        m2l_cl_between_groups.cpu_funcs[0] = M2LCallback<CellContainerClass>;
+        m2l_cl_between_groups.cpu_funcs[0] = &TbfSmStarpuCallbacks::M2LCallback<ThisClass, CellContainerClass>;
         m2l_cl_between_groups.where |= STARPU_CPU;
         m2l_cl_between_groups.nbuffers = 4;
         m2l_cl_between_groups.modes[0] = STARPU_R;
@@ -304,7 +117,7 @@ protected:
         m2l_cl_between_groups.name = "m2l_cl_between_groups";
 
         memset(&m2l_cl_inside, 0, sizeof(m2l_cl_inside));
-        m2l_cl_inside.cpu_funcs[0] = M2LInnerCallback<CellContainerClass>;
+        m2l_cl_inside.cpu_funcs[0] = &TbfSmStarpuCallbacks::M2LInnerCallback<ThisClass, CellContainerClass>;
         m2l_cl_inside.where |= STARPU_CPU;
         m2l_cl_inside.nbuffers = 3;
         m2l_cl_inside.modes[0] = STARPU_R;
