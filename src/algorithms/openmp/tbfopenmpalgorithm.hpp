47a152801cf7b7920625c49ad4d83c193b0bc2f4
Berenger Bramas <Berenger.Bramas@inria.fr>
2023-10-11 22:18:09 +0200

    update other algo P2P to match new order

--- src/algorithms/openmp/tbfopenmpalgorithm.hpp
+++ src/algorithms/openmp/tbfopenmpalgorithm.hpp
@@ -303,7 +303,7 @@ protected:
 
 #pragma omp task depend(in:ptr_groupSrcGetDataPtr[0],ptr_groupTargetGetDataPtr[0]) depend(commute:ptr_groupSrcGetRhsPtr[0],ptr_groupTargetGetRhsPtr[0]) default(shared) firstprivate(indexesVec, groupSrcPtr, groupTargetPtr) priority(priorities.getP2PPriority())
                 {
-                    kernelWrapper.P2PBetweenGroups(kernelsPtr[omp_get_thread_num()], *groupTargetPtr, *groupSrcPtr, std::move(*indexesVec));
+                    kernelWrapper.P2PBetweenGroups(kernelsPtr[omp_get_thread_num()], *groupSrcPtr, *groupTargetPtr, std::move(*indexesVec));
                     delete indexesVec;
                 }
             });
