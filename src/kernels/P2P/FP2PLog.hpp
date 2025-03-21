242036f1cb0832926fe3f11cbb3913d054abfc65
Rishat <karimr2000@mail.ru>
2025-03-21 21:37:43 +0300

    Added inastemp use
    (cherry picked from commit ea40d6b6258a5c9658b47f9cf8d28be1681dcafe)

--- src/kernels/P2P/FP2PLog.hpp
+++ src/kernels/P2P/FP2PLog.hpp
@@ -85,7 +85,7 @@ namespace FP2PLog
 
             for (long int idxSource = 0; idxSource < nbParticlesSources; ++idxSource)
             {
-                std::complex<FReal> source_point{targetsX[idxSource], targetsY[idxSource]};
+                std::complex<FReal> source_point{sourcesX[idxSource], sourcesY[idxSource]};
 
                 std::complex<FReal> distance{target_point - source_point};
                 std::complex<FReal> ln_distance{distance};
@@ -108,109 +108,53 @@ namespace FP2PLog
 
         const FReal *const targetsX = GetPtr(inTargets[0]);
         const FReal *const targetsY = GetPtr(inTargets[1]);
-        const FReal *const targetsZ = GetPtr(inTargets[2]);
-        const FReal *const targetsPhysicalValues = GetPtr(inTargets[3]);
-        FReal *const targetsForcesX = GetPtr(inTargetsRhs[0]);
-        FReal *const targetsForcesY = GetPtr(inTargetsRhs[1]);
-        FReal *const targetsForcesZ = GetPtr(inTargetsRhs[2]);
-        FReal *const targetsPotentials = GetPtr(inTargetsRhs[3]);
+        const FReal *const targetsPhysicalValues = GetPtr(inTargets[2]);
+        FReal *const targetsPotentials = GetPtr(inTargetsRhs[0]);
 
         const FReal *const sourcesX = GetPtr(inNeighbors[0]);
         const FReal *const sourcesY = GetPtr(inNeighbors[1]);
-        const FReal *const sourcesZ = GetPtr(inNeighbors[2]);
-        const FReal *const sourcesPhysicalValues = GetPtr(inNeighbors[3]);
-        FReal *const sourcesForcesX = GetPtr(inNeighborsRhs[0]);
-        FReal *const sourcesForcesY = GetPtr(inNeighborsRhs[1]);
-        FReal *const sourcesForcesZ = GetPtr(inNeighborsRhs[2]);
-        FReal *const sourcesPotentials = GetPtr(inNeighborsRhs[3]);
-
-        const VecType mOne = VecType(1);
+        const FReal *const sourcesPhysicalValues = GetPtr(inNeighbors[2]);
+        FReal *const sourcesPotentials = GetPtr(inNeighborsRhs[0]);
 
         for (long int idxTarget = 0; idxTarget < nbParticlesTargets; ++idxTarget)
         {
             const long int nbVectorizedInteractions = (nbParticlesSources / VecType::GetVecLength()) * VecType::GetVecLength();
             {
-                const VecType tx = VecType(targetsX[idxTarget]);
-                const VecType ty = VecType(targetsY[idxTarget]);
-                const VecType tz = VecType(targetsZ[idxTarget]);
+                const std::complex<VecType> target_point{VecType(targetsX[idxTarget]), VecType(targetsY[idxTarget])};
                 const VecType tv = VecType(targetsPhysicalValues[idxTarget]);
-                VecType tfx = VecType::GetZero();
-                VecType tfy = VecType::GetZero();
-                VecType tfz = VecType::GetZero();
                 VecType tpo = VecType::GetZero();
 
                 for (long int idxSource = 0; idxSource < nbVectorizedInteractions; idxSource += VecType::GetVecLength())
                 {
-                    VecType dx = VecType(&sourcesX[idxSource]) - tx;
-                    VecType dy = VecType(&sourcesY[idxSource]) - ty;
-                    VecType dz = VecType(&sourcesZ[idxSource]) - tz;
 
-                    VecType inv_square_distance = mOne / (dx * dx + dy * dy + dz * dz);
-                    const VecType inv_distance = inv_square_distance.sqrt();
+                    std::complex<VecType> source_point{VecType(&sourcesX[idxSource]), VecType(&sourcesY[idxSource])};
+                    std::complex<VecType> distance{target_point - source_point};
+                    std::complex<VecType> ln_distance{distance};
 
-                    inv_square_distance *= inv_distance;
-                    inv_square_distance *= tv * VecType(&sourcesPhysicalValues[idxSource]);
+                    tpo += ln_distance.real() * VecType(&sourcesPhysicalValues[idxSource]);
 
-                    dx *= inv_square_distance;
-                    dy *= inv_square_distance;
-                    dz *= inv_square_distance;
-
-                    tfx += dx;
-                    tfy += dy;
-                    tfz += dz;
-                    tpo += inv_distance * VecType(&sourcesPhysicalValues[idxSource]);
-
-                    (VecType(&sourcesForcesX[idxSource]) - dx).storeInArray(&sourcesForcesX[idxSource]);
-                    (VecType(&sourcesForcesY[idxSource]) - dy).storeInArray(&sourcesForcesY[idxSource]);
-                    (VecType(&sourcesForcesZ[idxSource]) - dz).storeInArray(&sourcesForcesZ[idxSource]);
-                    (VecType(&sourcesPotentials[idxSource]) + inv_distance * tv).storeInArray(&sourcesPotentials[idxSource]);
+                    (VecType(&sourcesPotentials[idxSource]) + ln_distance.real() * tv).storeInArray(&sourcesPotentials[idxSource]);
                 }
 
-                targetsForcesX[idxTarget] += (tfx.horizontalSum());
-                targetsForcesY[idxTarget] += (tfy.horizontalSum());
-                targetsForcesZ[idxTarget] += (tfz.horizontalSum());
                 targetsPotentials[idxTarget] += (tpo.horizontalSum());
             }
             {
-                const FReal tx = targetsX[idxTarget];
-                const FReal ty = targetsY[idxTarget];
-                const FReal tz = targetsZ[idxTarget];
+                std::complex<FReal> target_point{targetsX[idxTarget], targetsY[idxTarget]};
                 const FReal tv = targetsPhysicalValues[idxTarget];
-                FReal tfx = 0;
-                FReal tfy = 0;
-                FReal tfz = 0;
                 FReal tpo = 0;
 
                 for (long int idxSource = nbVectorizedInteractions; idxSource < nbParticlesSources; ++idxSource)
                 {
-                    FReal dx = sourcesX[idxSource] - tx;
-                    FReal dy = sourcesY[idxSource] - ty;
-                    FReal dz = sourcesZ[idxSource] - tz;
+                    std::complex<FReal> source_point{sourcesX[idxSource], sourcesY[idxSource]};
 
-                    FReal inv_square_distance = FReal(1.0) / (dx * dx + dy * dy + dz * dz);
-                    const FReal inv_distance = FMath::Sqrt(inv_square_distance);
+                    std::complex<FReal> distance{target_point - source_point};
+                    std::complex<FReal> ln_distance{distance};
 
-                    inv_square_distance *= inv_distance;
-                    inv_square_distance *= tv * sourcesPhysicalValues[idxSource];
+                    tpo += ln_distance.real() * sourcesPhysicalValues[idxSource];
 
-                    dx *= inv_square_distance;
-                    dy *= inv_square_distance;
-                    dz *= inv_square_distance;
-
-                    sourcesForcesX[idxSource] -= dx;
-                    sourcesForcesY[idxSource] -= dy;
-                    sourcesForcesZ[idxSource] -= dz;
-                    sourcesPotentials[idxSource] += inv_distance * tv;
-
-                    tfx += dx;
-                    tfy += dy;
-                    tfz += dz;
-                    tpo += inv_distance * sourcesPhysicalValues[idxSource];
+                    sourcesPotentials[idxSource] += ln_distance.real() * tv;
                 }
 
-                targetsForcesX[idxTarget] += (tfx);
-                targetsForcesY[idxTarget] += (tfy);
-                targetsForcesZ[idxTarget] += (tfz);
                 targetsPotentials[idxTarget] += (tpo);
             }
         }
@@ -256,100 +200,47 @@ namespace FP2PLog
 
         const FReal *const targetsX = GetPtr(inTargets[0]);
         const FReal *const targetsY = GetPtr(inTargets[1]);
-        const FReal *const targetsZ = GetPtr(inTargets[2]);
-        const FReal *const targetsPhysicalValues = GetPtr(inTargets[3]);
-        FReal *const targetsForcesX = GetPtr(inTargetsRhs[0]);
-        FReal *const targetsForcesY = GetPtr(inTargetsRhs[1]);
-        FReal *const targetsForcesZ = GetPtr(inTargetsRhs[2]);
-        FReal *const targetsPotentials = GetPtr(inTargetsRhs[3]);
-
-        const VecType mOne = VecType(1);
+        const FReal *const targetsPhysicalValues = GetPtr(inTargets[2]);
+        FReal *const targetsPotentials = GetPtr(inTargetsRhs[0]);
 
         for (long int idxTarget = 0; idxTarget < nbParticlesTargets; ++idxTarget)
         {
             const long int nbVectorizedInteractions = ((nbParticlesTargets - (idxTarget + 1)) / VecType::GetVecLength()) * VecType::GetVecLength() + (idxTarget + 1);
             {
-                const VecType tx = VecType(targetsX[idxTarget]);
-                const VecType ty = VecType(targetsY[idxTarget]);
-                const VecType tz = VecType(targetsZ[idxTarget]);
+                const std::complex<VecType> target_point{VecType(targetsX[idxTarget]), VecType(targetsY[idxTarget])};
                 const VecType tv = VecType(targetsPhysicalValues[idxTarget]);
-                VecType tfx = VecType::GetZero();
-                VecType tfy = VecType::GetZero();
-                VecType tfz = VecType::GetZero();
                 VecType tpo = VecType::GetZero();
 
                 for (long int idxSource = idxTarget + 1; idxSource < nbVectorizedInteractions; idxSource += VecType::GetVecLength())
                 {
-                    VecType dx = VecType(&targetsX[idxSource]) - tx;
-                    VecType dy = VecType(&targetsY[idxSource]) - ty;
-                    VecType dz = VecType(&targetsZ[idxSource]) - tz;
 
-                    VecType inv_square_distance = mOne / (dx * dx + dy * dy + dz * dz);
-                    const VecType inv_distance = inv_square_distance.sqrt();
+                    std::complex<VecType> source_point{VecType(&targetsX[idxSource]), VecType(&targetsY[idxSource])};
 
-                    inv_square_distance *= inv_distance;
-                    inv_square_distance *= tv * VecType(&targetsPhysicalValues[idxSource]);
+                    std::complex<FReal> distance{target_point - source_point};
+                    std::complex<FReal> ln_distance{distance};
 
-                    dx *= inv_square_distance;
-                    dy *= inv_square_distance;
-                    dz *= inv_square_distance;
+                    tpo += ln_distance.real() * VecType(&targetsPhysicalValues[idxSource]);
 
-                    tfx += dx;
-                    tfy += dy;
-                    tfz += dz;
-                    tpo += inv_distance * VecType(&targetsPhysicalValues[idxSource]);
-
-                    (VecType(&targetsForcesX[idxSource]) - dx).storeInArray(&targetsForcesX[idxSource]);
-                    (VecType(&targetsForcesY[idxSource]) - dy).storeInArray(&targetsForcesY[idxSource]);
-                    (VecType(&targetsForcesZ[idxSource]) - dz).storeInArray(&targetsForcesZ[idxSource]);
-                    (VecType(&targetsPotentials[idxSource]) + inv_distance * tv).storeInArray(&targetsPotentials[idxSource]);
+                    (VecType(&targetsPotentials[idxSource]) + ln_distance.real() * tv).storeInArray(&targetsPotentials[idxSource]);
                 }
 
-                targetsForcesX[idxTarget] += (tfx.horizontalSum());
-                targetsForcesY[idxTarget] += (tfy.horizontalSum());
-                targetsForcesZ[idxTarget] += (tfz.horizontalSum());
                 targetsPotentials[idxTarget] += (tpo.horizontalSum());
             }
             {
-                const FReal tx = targetsX[idxTarget];
-                const FReal ty = targetsY[idxTarget];
-                const FReal tz = targetsZ[idxTarget];
+
+                std::complex<FReal> target_point{targetsX[idxTarget], targetsY[idxTarget]};
                 const FReal tv = targetsPhysicalValues[idxTarget];
-                FReal tfx = 0;
-                FReal tfy = 0;
-                FReal tfz = 0;
                 FReal tpo = 0;
 
                 for (long int idxSource = nbVectorizedInteractions; idxSource < nbParticlesTargets; ++idxSource)
                 {
-                    FReal dx = targetsX[idxSource] - tx;
-                    FReal dy = targetsY[idxSource] - ty;
-                    FReal dz = targetsZ[idxSource] - tz;
+                    std::complex<FReal> source_point{sourcesX[idxSource], sourcesY[idxSource]};
+                    std::complex<FReal> distance{target_point - source_point};
+                    std::complex<FReal> ln_distance{distance};
 
-                    FReal inv_square_distance = FReal(1.0) / (dx * dx + dy * dy + dz * dz);
-                    const FReal inv_distance = FMath::Sqrt(inv_square_distance);
-
-                    inv_square_distance *= inv_distance;
-                    inv_square_distance *= tv * targetsPhysicalValues[idxSource];
-
-                    dx *= inv_square_distance;
-                    dy *= inv_square_distance;
-                    dz *= inv_square_distance;
-
-                    targetsForcesX[idxSource] -= dx;
-                    targetsForcesY[idxSource] -= dy;
-                    targetsForcesZ[idxSource] -= dz;
-                    targetsPotentials[idxSource] += inv_distance * tv;
-
-                    tfx += dx;
-                    tfy += dy;
-                    tfz += dz;
-                    tpo += inv_distance * targetsPhysicalValues[idxSource];
+                    tpo += ln_distance.real() * sourcesPhysicalValues[idxSource];
                 }
 
-                targetsForcesX[idxTarget] += (tfx);
-                targetsForcesY[idxTarget] += (tfy);
-                targetsForcesZ[idxTarget] += (tfz);
                 targetsPotentials[idxTarget] += (tpo);
             }
         }
@@ -400,96 +291,47 @@ namespace FP2PLog
 
         const FReal *const targetsX = GetPtr(inTargets[0]);
         const FReal *const targetsY = GetPtr(inTargets[1]);
-        const FReal *const targetsZ = GetPtr(inTargets[2]);
-        const FReal *const targetsPhysicalValues = GetPtr(inTargets[3]);
-        FReal *const targetsForcesX = GetPtr(inTargetsRhs[0]);
-        FReal *const targetsForcesY = GetPtr(inTargetsRhs[1]);
-        FReal *const targetsForcesZ = GetPtr(inTargetsRhs[2]);
-        FReal *const targetsPotentials = GetPtr(inTargetsRhs[3]);
-
-        const VecType mOne = VecType(1);
+        const FReal *const targetsPhysicalValues = GetPtr(inTargets[2]);
+        FReal *const targetsPotentials = GetPtr(inTargetsRhs[0]);
 
         const FReal *const sourcesX = GetPtr(inNeighbors[0]);
         const FReal *const sourcesY = GetPtr(inNeighbors[1]);
-        const FReal *const sourcesZ = GetPtr(inNeighbors[2]);
-        const FReal *const sourcesPhysicalValues = GetPtr(inNeighbors[3]);
+        const FReal *const sourcesPhysicalValues = GetPtr(inNeighbors[2]);
 
         const long int nbVectorizedInteractionsSource = (nbParticlesSources / VecType::GetVecLength()) * VecType::GetVecLength();
 
         for (long int idxTarget = 0; idxTarget < nbParticlesTargets; ++idxTarget)
         {
             {
-                const VecType tx = VecType(targetsX[idxTarget]);
-                const VecType ty = VecType(targetsY[idxTarget]);
-                const VecType tz = VecType(targetsZ[idxTarget]);
+                const std::complex<VecType> target_point{VecType(targetsX[idxTarget]), VecType(targetsY[idxTarget])};
                 const VecType tv = VecType(targetsPhysicalValues[idxTarget]);
-                VecType tfx = VecType::GetZero();
-                VecType tfy = VecType::GetZero();
-                VecType tfz = VecType::GetZero();
                 VecType tpo = VecType::GetZero();
 
                 for (long int idxSource = 0; idxSource < nbVectorizedInteractionsSource; idxSource += VecType::GetVecLength())
                 {
-                    VecType dx = VecType(&sourcesX[idxSource]) - tx;
-                    VecType dy = VecType(&sourcesY[idxSource]) - ty;
-                    VecType dz = VecType(&sourcesZ[idxSource]) - tz;
+                    std::complex<VecType> source_point{VecType(&sourcesX[idxSource]), VecType(&sourcesY[idxSource])};
+                    std::complex<VecType> distance{target_point - source_point};
+                    std::complex<VecType> ln_distance{distance};
 
-                    VecType inv_square_distance = mOne / (dx * dx + dy * dy + dz * dz);
-                    const VecType inv_distance = inv_square_distance.sqrt();
-
-                    inv_square_distance *= inv_distance;
-                    inv_square_distance *= tv * VecType(&sourcesPhysicalValues[idxSource]);
-
-                    dx *= inv_square_distance;
-                    dy *= inv_square_distance;
-                    dz *= inv_square_distance;
-
-                    tfx += dx;
-                    tfy += dy;
-                    tfz += dz;
-                    tpo += inv_distance * VecType(&sourcesPhysicalValues[idxSource]);
+                    tpo += ln_distance.real() * VecType(&sourcesPhysicalValues[idxSource]);
                 }
 
-                targetsForcesX[idxTarget] += (tfx.horizontalSum());
-                targetsForcesY[idxTarget] += (tfy.horizontalSum());
-                targetsForcesZ[idxTarget] += (tfz.horizontalSum());
                 targetsPotentials[idxTarget] += (tpo.horizontalSum());
             }
             {
-                const FReal tx = targetsX[idxTarget];
-                const FReal ty = targetsY[idxTarget];
-                const FReal tz = targetsZ[idxTarget];
+
                 const FReal tv = targetsPhysicalValues[idxTarget];
-                FReal tfx = 0;
-                FReal tfy = 0;
-                FReal tfz = 0;
                 FReal tpo = 0;
 
                 for (long int idxSource = nbVectorizedInteractionsSource; idxSource < nbParticlesSources; ++idxSource)
                 {
-                    FReal dx = sourcesX[idxSource] - tx;
-                    FReal dy = sourcesY[idxSource] - ty;
-                    FReal dz = sourcesZ[idxSource] - tz;
+                    std::complex<FReal> source_point{sourcesX[idxSource], sourcesY[idxSource]};
+                    std::complex<FReal> distance{target_point - source_point};
+                    std::complex<FReal> ln_distance{distance};
 
-                    FReal inv_square_distance = FReal(1) / (dx * dx + dy * dy + dz * dz);
-                    const FReal inv_distance = FMath::Sqrt(inv_square_distance);
-
-                    inv_square_distance *= inv_distance;
-                    inv_square_distance *= tv * sourcesPhysicalValues[idxSource];
-
-                    dx *= inv_square_distance;
-                    dy *= inv_square_distance;
-                    dz *= inv_square_distance;
-
-                    tfx += dx;
-                    tfy += dy;
-                    tfz += dz;
-                    tpo += inv_distance * sourcesPhysicalValues[idxSource];
+                    tpo += ln_distance.real() * sourcesPhysicalValues[idxSource];
                 }
 
-                targetsForcesX[idxTarget] += (tfx);
-                targetsForcesY[idxTarget] += (tfy);
-                targetsForcesZ[idxTarget] += (tfz);
                 targetsPotentials[idxTarget] += (tpo);
             }
         }
